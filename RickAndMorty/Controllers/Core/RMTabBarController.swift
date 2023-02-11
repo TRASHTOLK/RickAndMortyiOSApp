4c21dfbe1bc3e2894145485bac367339fb5d17d8
LevkovichKirillx <levkovich.kirill.1@mail.ru>
2023-02-11 11:34:04 +0300

    Create Rick&Morty project

---
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ya on 10.02.23.
+//
+
+import UIKit
+
+class RMTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+    
+    private func setUpTabs() {
+        let characterVC = 
+    }
+
+}
+
---
+++ RickAndMorty/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  RickAndMorty
+//
+//  Created by Ya on 10.02.23.
+//
+
+import Foundation
